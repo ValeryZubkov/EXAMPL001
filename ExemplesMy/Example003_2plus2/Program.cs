0104ba02f0024fd7cb5a73b2e3804d78de0a1032
ValeryZubkov <109296518+ValeryZubkov@users.noreply.github.com>
2022-08-04 18:33:44 +0400

    add project sum

---
+++ ExemplesMy/Example003_2plus2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Введите 1-е число:");
+//int numberA = Console.Read();
+//Console.WriteLine("Введите 2-е число:");
+//int numberB = Console.Read();
+int numberA = 35;
+int numberB = 3;
+int sum = numberA + numberB;
+Console.WriteLine("Сумма двух чисел будет равна: " + sum);
